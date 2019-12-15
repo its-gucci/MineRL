f69cfc141ba7711929c9b1e2c7fbf9f200923009
Jeffrey Gu <jeffgu@stanford.edu>
2019-12-14 22:54:36 -0500

    add new commands for running remotely

--- dapg/run.sh
+++ dapg/run.sh
@@ -1,8 +1,11 @@
 # debug model 
-python3 train_agent.py --pretrain 1 --bc 1 --episodes 1 --eval --eval-runs 1
+# python3 train_agent.py --pretrain 1 --bc 1 --episodes 1 --eval --eval-runs 1
 
 # train model with no loading
 # python3 train_agent.py
 
 # load model and eval with no training
 # python3 train_agent.py -l --eval 
+
+# debug model on GCloud
+python3 train_agent.py --pretrain 1 --bc 1 --episodes 1 --eval --eval-runs 1 --datadir '../distributed_rl/minerl_data/datasets'
